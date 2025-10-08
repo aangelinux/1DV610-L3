6edeefc7a89d707c549f1bd061ee3716bffc93d7
Angelina Larsson <al227bn@student.lnu.se>
2025-10-08 08:21:17 +0200

    Add region configs

--- src/config/regions.js
+++ src/config/regions.js
@@ -14,33 +14,117 @@ export class RegionConfig {
 	]
 
 	#EAS = [
-		
+		"China",
+		"Indonesia",
+		"Japan",
+		"Philippines",
+		"Viet Nam",
+		"Thailand",
+		"Myanmar",
+		"Korea, Rep.",  // South
+		"Malaysia",
+		"Australia"
 	]
 
 	#ECS = [
-
+		"Russian Fed",
+		"Türkiye",
+		"Germany",
+		"United Kingdom",
+		"France",
+		"Italy",
+		"Spain",
+		"Ukraine",
+		"Poland",
+		"Sweden"
 	]
 
 	#LCN = [
-
+		"Brazil",
+		"Mexico",
+		"Colombia",
+		"Argentina",
+		"Peru",
+		"Venezuela",
+		"Chile",
+		"Guatemala",
+		"Ecuador",
+		"Bolivia"
 	]
 
 	#MEA = [
-
+		"Pakistan",
+		"Egypt",
+		"Iran",
+		"Algeria",
+		"Iraq",
+		"Afghanistan",
+		"Yemen, Rep.",
+		"Morocco",
+		"Saudi Arabia",
+		"Syria"
 	]
 
 	#NAC = [
-
+		"Canada",
+		"United States",
+		"Bermuda"
 	]
 
 	#SAS = [
-
+		"Bangladesh",
+		"Bhutan",
+		"India",
+		"Maldives",
+		"Nepal",
+		"Sri Lanka"
 	]
 
 	#SSF = [
-
+		"Nigeria",
+		"Ethiopia",
+		"Congo, Dem.",
+		"Tanzania",
+		"South Africa",
+		"Kenya",
+		"Sudan",
+		"Uganda",
+		"Angola",
+		"Mozambique"
 	]
 
 	constructor() {
+	}
+
+	get global() {
+		return this.#GLOBAL
+	}
+
+	get eas() {
+		return this.#EAS
+	}
+
+	get ecs() {
+		return this.#ECS
+	}
+
+	get lcn() {
+		return this.#LCN
+	}
+
+	get mea() {
+		return this.#MEA
+	}
+
+	get nac() {
+		return this.#NAC
+	}
+
+	get sas() {
+		return this.#SAS
+	}
+
+	get ssf() {
+		return this.#SSF
 	}
 }
