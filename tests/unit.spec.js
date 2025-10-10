64d071683539e2923fe9a6b8f973721b49409130
Angelina Larsson <al227bn@student.lnu.se>
2025-10-10 16:05:25 +0200

    Set up test environment

---
+++ tests/unit.spec.js
@@ -0,0 +1,11 @@
+/**
+ * @module Defines unit tests for the application.
+ */
+
+import { describe, test, expect } from "@jest/globals"
+
+import { DataParser } from "../src/utils/dataParser.js"
+import { RegionConfig } from "../src/config/regions.js"
+import { ChartConfig } from "../src/config/charts.js"
+
+describe("")
